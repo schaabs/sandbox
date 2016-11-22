540339fb04e5a98755d5254a9e903419177e5090
sschaab <sschaab@microsoft.com>
2016-11-21 16:29:21 -0800

    adding netcore sandbox project

--- src/sandbox/Program.cs
+++ src/sandbox/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using sandbox.tools;
 using System.IO;
+using stress.codegen;
 
 namespace sandbox.temp
 {
@@ -17,7 +18,24 @@ namespace sandbox.temp
 
         static void Run()
         {
-            Console.WriteLine("Hello World");
+            var lti = new LoadTestInfo("")
+            {
+                SourceDirectory = @"d:\temp\sandbox\out",
+                TestName = "HelloWorld_0001",
+                EnvironmentVariables = new Dictionary<string, string>()
+                {
+                    { "Foo",  "1" },
+                    { "Bar",  "3" }
+                },
+                SuiteConfig = new LoadSuiteConfig() { Host = "corerun" },
+
+            };
+
+
+
+            var shgen = new ExecutionFileGeneratorLinux();
+
+            shgen.GenerateSourceFile(lti);
         }
     }
 
