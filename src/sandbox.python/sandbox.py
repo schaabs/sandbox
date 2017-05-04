9266c3b7248910f1124707b8de38702674266716
sschaab <sschaab@microsoft.com>
2017-05-04 14:37:59 -0700

    backup commit

--- src/sandbox.python/sandbox.py
+++ src/sandbox.python/sandbox.py
@@ -1,16 +1,14 @@
 class Foo(object):
-    def __init__(self, message):
-        self.message = message
-
-
-class Bar(Foo):
     def __init__(self):
-        self.ps = ' ttfn'
+        pass
 
-    def display_message(self):
-        print('%s %s'%(self.message, self.ps))
+    def greet(self, person, adj, **opargs):
+        print('Hello %s, %s' % (person, adj))
+
+    def greet(self, person, **opargs):
+        print('Hello %s' % person)
 
 if __name__ == '__main__':
-    f = Foo("Hello World")
-    f.__class__ = Bar
-    f.display_message()
+    f = Foo()
+    f.greet('Double D')
+    f.greet('Double D', 'asshole')
