c9082dd34583cd7ad40caf90546eec990b11db78
sschaab <sschaab@microsoft.com>
2017-05-31 12:59:16 -0700

    playing with python decorator methods

--- src/sandbox.python/sandbox.py
+++ src/sandbox.python/sandbox.py
@@ -1,14 +1,33 @@
+import inspect
+
+
+def build_vault_impl(vault_name, secret):
+    print('creating vault: %s with secret %s'%(vault_name, secret))
+
+def build_vault(f):
+    vault_name = f.__name__
+
+    def wrapper(self):
+        build_vault_impl(vault_name, self.secret)
+        f(self, vault_name)
+    return wrapper
+
+
 class Foo(object):
-    def __init__(self):
-        pass
+    def __init__(self, secret):
+        self.secret = secret
 
-    def greet(self, person, adj, **opargs):
-        print('Hello %s, %s' % (person, adj))
+    @build_vault
+    def test1(self, vault_name=None):
+        print('executing test1 with vault %s'%vault_name);
 
-    def greet(self, person, **opargs):
-        print('Hello %s' % person)
+class Bar(Foo):
+    @build_vault
+    def test2(self, vault_name=None):
+        print('executing test2 with vault %s'%vault_name);
 
 if __name__ == '__main__':
-    f = Foo()
-    f.greet('Double D')
-    f.greet('Double D', 'asshole')
+    f = Foo('password')
+    b = Bar('password')
+    f.test1()
+    b.test2()
