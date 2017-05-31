0715106dd5f9d60934b3f0ce7e5a1953eaef37e6
sschaab <sschaab@microsoft.com>
2017-05-31 13:31:13 -0700

    updating decorator experiment to work inside unittest

--- src/sandbox.python/sandbox.py
+++ src/sandbox.python/sandbox.py
@@ -1,33 +1,35 @@
-import inspect
+import unittest
 
 
 def build_vault_impl(vault_name, secret):
     print('creating vault: %s with secret %s'%(vault_name, secret))
 
-def build_vault(f):
-    vault_name = f.__name__
 
+def build_vault(f):
+    test_friendly_name = f.__name__.replace('_', '-')
+    vault_name = test_friendly_name + '-vault'
     def wrapper(self):
         build_vault_impl(vault_name, self.secret)
         f(self, vault_name)
     return wrapper
 
 
-class Foo(object):
-    def __init__(self, secret):
-        self.secret = secret
+class Foo(unittest.TestCase):
+    def setUp(self):
+        self.secret = 'password1'
 
     @build_vault
-    def test1(self, vault_name=None):
-        print('executing test1 with vault %s'%vault_name);
+    def test_1(self, vault_name=None):
+        print('executing test1 with vault %s'%vault_name)
 
-class Bar(Foo):
+
+class Bar(unittest.TestCase):
+    def setUp(self):
+        self.secret = 'password2'
+
     @build_vault
-    def test2(self, vault_name=None):
-        print('executing test2 with vault %s'%vault_name);
+    def test_2(self, vault_name=None):
+        print('executing test2 with vault %s'%vault_name)
 
 if __name__ == '__main__':
-    f = Foo('password')
-    b = Bar('password')
-    f.test1()
-    b.test2()
+    unittest.main()
