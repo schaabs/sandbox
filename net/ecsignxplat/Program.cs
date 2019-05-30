b07a53673d8553f42879c2dbe6cfb27601ed29c6
Scott Schaab <sschaab@microsoft.com>
2019-05-30 00:25:44 -0700

    quick ec xplat test

---
+++ net/ecsignxplat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ecsignxplat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = Encoding.Unicode.GetBytes("I got's some data and I'm fixin' to get it signed");
+
+            var ecKey = ECDsa.Create();
+
+            ecKey.GenerateKey(ECCurve.CreateFromFriendlyName("secp256k1"));
+
+            byte[] signature = ecKey.SignData(data, HashAlgorithmName.SHA256);
+
+            Console.WriteLine(BitConverter.ToString(signature).Replace("-", ""));
+        }
+    }
+}
